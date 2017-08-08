2add28717cc7571a0aea4389d67e01fb95760298
kvzhao <kelispinor@gmail.com>
2017-08-08 17:49:00 +0800

    Set data normalization as an option

--- constants.py
+++ constants.py
@@ -1,12 +1,14 @@
 
 DATA_PATH = "datasetCoeffs/train.txt"
 LABEL_PATH = "datasetCoeffs/label.txt"
+DATA_NORMALIZED = False
+
 # ~ 2000 steps per epoch
 NUM_EPOCH = 2000
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 
 # batch size of testing data
 TEST_BATCH_SIZE = 1024
 
 # records summaries per steps
-EVAL_PER_STEPS = 1000
+EVAL_PER_STEPS = 500
