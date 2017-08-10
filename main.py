9228850fffe10f7a31c97c7e1d6ac3aabfb9f189
kvzhao <kelispinor@gmail.com>
2017-08-10 17:13:33 +0800

    More specific usage of RNN training

---
+++ main.py
@@ -0,0 +1,205 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import numpy as np
+import time
+import tensorflow as tf
+import tensorflow.contrib.rnn as rnn
+
+from data_sampler import DataSampler
+from rnn_model import RNN
+
+task_name = 'trial'
+
+print ('Version of tensorflow is {}'.format(tf.__version__))
+
+# management
+tf.app.flags.DEFINE_bool("is_train", True, "Set true for training, false flag will launch testing and analysis")
+tf.app.flags.DEFINE_string("task_name", "trial", "Assign objective of task")
+tf.app.flags.DEFINE_string("DATA_PATH", "datasetConfig/states.txt", "Set path of states file")
+tf.app.flags.DEFINE_string("LABEL_PATH", "datasetConfig/sign.txt", "Path to file of sign")
+
+#TODO: Add reset option
+
+# hyper-params
+tf.app.flags.DEFINE_float("learning_rate", 0.001, "Learning rate.")
+tf.app.flags.DEFINE_integer("cell_size", 8, "Size of lstm cells")
+tf.app.flags.DEFINE_integer("num_output", 2, "Number of classes")
+
+# traning process
+tf.app.flags.DEFINE_integer("NUM_EPOCH", 200, "Number of epochs")
+tf.app.flags.DEFINE_integer("BATCH_SIZE", 32, "Batch size")
+tf.app.flags.DEFINE_integer("TEST_BATCH_SIZE", 512, "Test Batch size")
+tf.app.flags.DEFINE_integer("EVAL_PER_STEPS", 2000, "Steps between evaluation")
+tf.app.flags.DEFINE_integer("SAVE_CKPT_PER_STEPS", 100000, "Steps interval for saving checkpoints")
+
+FLAGS = tf.app.flags.FLAGS
+
+gpu_options = tf.GPUOptions(allow_growth=True)
+
+def get_weights_by_name (sess, name):
+    var = [v for v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES) if v.name.endswith(name)]
+    return np.squeeze(sess.run(var))
+
+# add task_name parser
+
+"""
+    MAIN PROGRAM
+"""
+
+with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+    data_sampler = DataSampler(FLAGS.DATA_PATH, FLAGS.LABEL_PATH)
+    num_train_data = data_sampler.num_train
+    num_test_data  = data_sampler.num_test
+
+    task_name = FLAGS.task_name + '_' + 'x'.join([str(FLAGS.cell_size),
+                                                    str(FLAGS.num_output),
+                                                    str(FLAGS.learning_rate)])
+
+    logfile = '/'.join(['logs', task_name])
+    ckptfile = '/'.join(['checkpoints', task_name])
+    if not tf.gfile.Exists(logfile):
+        tf.gfile.MakeDirs(logfile)
+    if not tf.gfile.Exists(ckptfile):
+        tf.gfile.MakeDirs(ckptfile)
+
+    if FLAGS.is_train:
+        print ('Training')
+        
+        x = tf.placeholder(tf.float32, [None, data_sampler.x_dim] , name='x')
+        y = tf.placeholder(tf.int32, [None, data_sampler.n_classes], name='y')
+
+        net = RNN(x, cell_size=FLAGS.cell_size, out_size=FLAGS.num_output)
+        logits, _ = net()
+
+        # loss function
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+        correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        confusion_matrix = tf.contrib.metrics.confusion_matrix(labels=tf.argmax(y, 1), predictions=tf.argmax(logits, 1)) 
+
+        # Solver
+        solver = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate).minimize(loss, var_list=net.vars)
+
+        # Saver
+        saver = tf.train.Saver()
+        writer = tf.summary.FileWriter(logfile, sess.graph)
+
+        with tf.name_scope('summaries'):
+            loss_sum = tf.summary.scalar('loss', loss)
+            acc_sum = tf.summary.scalar('accuracy', accuracy)
+            summary_op = tf.summary.merge_all()
+
+        # Checkpoints
+        checkpoint = tf.train.get_checkpoint_state(ckptfile)
+        if checkpoint and checkpoint.model_checkpoint_path:
+            saver.restore(sess, checkpoint.model_checkpoint_path)
+            print ('Restore checkpoints from {}'.format(checkpoint.model_checkpoint_path))
+        else:
+            print ('Can not find checkpoint files, run new experiment')
+
+        # training loop
+        start_time = time.time()
+        stable_counter = 0
+
+        STEPS_PER_EPOCH = int (num_train_data/FLAGS.BATCH_SIZE)
+        print ('{} steps per epoch'.format(STEPS_PER_EPOCH))
+
+        # initialize the variables before running expierment
+        sess.run(tf.global_variables_initializer())
+
+        for t in range(0, FLAGS.NUM_EPOCH * STEPS_PER_EPOCH):
+            batch_x, batch_y = data_sampler(FLAGS.BATCH_SIZE, is_train=True)
+            _, cost = sess.run([solver, loss], feed_dict={y: batch_y, x: batch_x})
+            if t % FLAGS.EVAL_PER_STEPS == 0:
+                losses = sess.run([loss], feed_dict={y: batch_y, x: batch_x})
+                print('Iter [%8d] Time [%5.4f] Training Loss = %.4f ' % (t, time.time() - start_time, losses[0]))
+            if t % FLAGS.SAVE_CKPT_PER_STEPS == 0:
+                print ('Save to checkpoint')
+                saver.save(sess, ckptfile + '/model', global_step=t)
+                # evaluation per epoch
+            if t % STEPS_PER_EPOCH == 0:
+                test_batch_x, test_batch_y = data_sampler(FLAGS.TEST_BATCH_SIZE, is_train=False)
+                losses, acc, summary, confmat = sess.run([loss, accuracy, summary_op, confusion_matrix],
+                                                            feed_dict={x: test_batch_x, y: test_batch_y})
+                writer.add_summary(summary, global_step=t)
+                print('Iter [%8d] Time [%5.4f] Validation Accuracy = %.4f' % (t, time.time() - start_time, acc))
+                print('Confusion : \n\t{} \n\t{}'.format(confmat[0], confmat[1]))
+                if (acc >= 0.97):
+                    stable_counter += 1
+                    if (stable_counter >= 1000):
+                        print ('Validation Accuracy exceeds 97%, the task terminates.')
+                        break
+    else: # is_train=false
+        print ('Testing')
+
+        x = tf.placeholder(tf.float32, [None, data_sampler.x_dim] , name='x')
+        y = tf.placeholder(tf.int32, [None, data_sampler.n_classes], name='y')
+
+        # allocate empty network
+        net = RNN(x, cell_size=FLAGS.cell_size, out_size=FLAGS.num_output)
+        # connect operators
+        logits, cell_states = net()
+        prob_op = tf.nn.softmax (logits)
+
+        # initialization  (not necessary)
+        sess.run(tf.global_variables_initializer())
+
+        # Load checkpoints
+        checkpoint = tf.train.get_checkpoint_state(ckptfile)
+        saver = tf.train.Saver()
+        if checkpoint and checkpoint.model_checkpoint_path:
+            saver.restore(sess, checkpoint.model_checkpoint_path)
+            var_names =  [v for v in tf.global_variables()]
+            print ('Restore checkpoints from {} with {} trained variables'.format(checkpoint.model_checkpoint_path, len(var_names)))
+            for var in var_names:
+                print (var)
+        else:
+            sys.exit ('Can not running test without trained model, please train first, use option --is_train=True')
+
+        correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        confusion_matrix = tf.contrib.metrics.confusion_matrix(labels=tf.argmax(y, 1), predictions=tf.argmax(logits, 1)) 
+
+        start_time = time.time()
+
+        test_batch_x, test_batch_y = data_sampler(FLAGS.TEST_BATCH_SIZE, is_train=False)
+        probs, acc, confmat, lstm_states = sess.run([prob_op, accuracy, confusion_matrix, cell_states], feed_dict={x: test_batch_x, y: test_batch_y}) 
+
+        print('Prediction Time = {}, Validation Accuracy = {} %'.format((time.time() - start_time), acc * 100.0))
+        print('Confusion Matrix : \n\t{} \n\t{}'.format(confmat[0], confmat[1]))
+
+        """
+            THE FOLLOWING CODES ARE USEFUL FOR FURTHER ANALYSIS
+        """
+
+        # shape [batch_size, cell_size]
+        lstm_h = lstm_states[0]
+        lstm_c = lstm_states[1]
+        
+        ## retrieve weights from loaded variables
+        lstm_mat = get_weights_by_name(sess, 'kernel:0')
+        lstm_bias =get_weights_by_name(sess, 'bias:0') 
+        linout_w = get_weights_by_name(sess, 'w:0')
+        linout_b = get_weights_by_name(sess, 'b:0')
+
+        """
+            Meaning of subscript
+            i: information gate
+            c: memory gate
+            f: forget gate
+            o: output gate
+        """
+        Wi, Wc, Wf, Wo = np.hsplit(lstm_mat, 4)
+        bi, bc, bf, bo = np.split(lstm_bias, 4)
+
+        print (Wi.shape)
+        print (Wc.shape)
+        print (Wf.shape)
+        print (Wo.shape)
+        print (bi.shape)
+        print (bc.shape)
+        print (bf.shape)
+        print (bo.shape)
+        print (linout_w.shape)
+        print (linout_b.shape)
---
+++ rnn_model.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import tensorflow.contrib.rnn as rnn
+
+def variable_summaries(var, name):
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar(name+'_mean', mean)
+        tf.summary.scalar(name+'_sparsity', tf.nn.zero_fraction(var))
+    with tf.name_scope('stddev'):
+        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar(name+'_stddev', stddev)
+    tf.summary.scalar(name+'_max', tf.reduce_max(var))
+    tf.summary.scalar(name+'_min', tf.reduce_min(var))
+    tf.summary.histogram(name+'_histogram', var)
+
+class RNN(object):
+    def __init__ (self, x, cell_size, out_size, write_summary=True, name='rnn_net', reuse=False):
+        self.name = name
+        self.cell_size = cell_size
+        self.out_size = out_size
+        self.x = x
+        self.reuse = reuse
+        self.write_summary = write_summary
+
+        self._build()
+
+    def _build(self):
+        with tf.variable_scope(self.name, reuse=self.reuse):
+            print ('Building RNN network')
+
+            lstm_cell = rnn.BasicLSTMCell(self.cell_size, forget_bias=1.0, 
+                                        reuse=tf.get_variable_scope().reuse)
+            outputs, states = rnn.static_rnn(lstm_cell, [self.x], dtype=tf.float32)
+            outshape = outputs[-1].get_shape()
+            w = tf.get_variable("w", [outshape[1], self.out_size], initializer=tf.random_normal_initializer())
+            b = tf.get_variable("b", [self.out_size], initializer=tf.constant_initializer(0.001))
+            linout = tf.matmul(outputs[-1], w) + b
+            if (self.write_summary):
+                variable_summaries(w, self.name + '_w')
+                variable_summaries(b, self.name + '_b')
+            self.logits = linout
+            self.lstm_states = states
+
+    def __call__(self):
+        return self.logits, self.lstm_states
+
+    @property
+    def vars(self):
+        return [var for var in tf.global_variables() if self.name in var.name]
