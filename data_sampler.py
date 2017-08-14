19de54c67cfa9b15d642cf63e9232032ea829863
kvzhao <kelispinor@gmail.com>
2017-08-14 15:53:25 +0800

    Control ratio of training set and testing set

--- data_sampler.py
+++ data_sampler.py
@@ -4,13 +4,13 @@ import collections
 
 from random import shuffle
 
-def read_data_sets(data_path, label_path, normalized=True):
+def read_data_sets(data_path, label_path, trainset_ratio, normalized=True):
     ## read data, hard-coded
     images = np.loadtxt(data_path)
     labels = np.loadtxt(label_path)
     num_data = images.shape[0]
-    ## 80% for training and 20% data for testing
-    train_num = int(.8 * num_data)
+    ## 80% for training and 20% data for testing by default
+    train_num = int(trainset_ratio * num_data)
     # shuffle and seperate training and testing set
     perm = np.arange(num_data)
     np.random.shuffle(perm)
@@ -25,9 +25,10 @@ def read_data_sets(data_path, label_path, normalized=True):
     return trainset, testset
 
 class DataSampler(object):
-    def __init__ (self, DATA_PATH, LABEL_PATH, DATA_NORMALIZED=False):
+    def __init__ (self, DATA_PATH, LABEL_PATH, TRAINSET_RATIO=0.8, DATA_NORMALIZED=False):
         #TODO: dont use hard-coded
-        self.train_set, self.test_set = read_data_sets(DATA_PATH, LABEL_PATH, normalized=DATA_NORMALIZED)
+        self.train_set, self.test_set = read_data_sets(DATA_PATH, LABEL_PATH,
+                                        trainset_ratio=TRAINSET_RATIO, normalized=DATA_NORMALIZED)
         self.num_train = self.train_set._num_of_samples
         self.num_test  = self.test_set._num_of_samples
         self.x_dim = self.train_set._image_shape[0]
@@ -39,7 +40,7 @@ class DataSampler(object):
             return self.train_set.next_batch(batch_size)
         else:
             return self.test_set.next_batch(batch_size)
-    
+
 class DataSet(object):
     def __init__(self, images, labels, dtpye=np.float32, normalized=True):
         self._images = images
@@ -74,7 +75,7 @@ class DataSet(object):
         mean = np.mean(self._images)
         stddev = np.std(self._images)
         print ('[DS] remove zero, mean = {}, and std = {}'.format(mean, stddev))
-        # convert 0 to -1 
+        # convert 0 to -1
         self._images = (self._images - mean) / stddev
         print ('[DS] processed dataset with mean = {}, and std = {}'.format(np.mean(self._images), np.std(self._images)))
         #  And shuffle the data
